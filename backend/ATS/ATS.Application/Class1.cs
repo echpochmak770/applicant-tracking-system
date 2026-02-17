1930de3a7b3566239e90b3ef4ca892b4efd0b1de
echpochmak770 <echpochmak770@gmail.com>
2026-02-17 17:20:46 +0300

    Setup backend projects and Swagger

---
+++ backend/ATS/ATS.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ATS.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ backend/ATS/ATS.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATS.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+
+
+    }
+}
