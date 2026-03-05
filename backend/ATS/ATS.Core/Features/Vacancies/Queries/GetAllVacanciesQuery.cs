bd73090e06665ac89b198b4520dcb586d055eb05
echpochmak770 <echpochmak770@gmail.com>
2026-03-05 17:04:55 +0300

    deleted mistakenly added property from GetAllVacanciesQuery

--- backend/ATS/ATS.Core/Features/Vacancies/Queries/GetAllVacanciesQuery.cs
+++ backend/ATS/ATS.Core/Features/Vacancies/Queries/GetAllVacanciesQuery.cs
@@ -6,6 +6,5 @@ namespace ATS.Core.Features.Vacancies.Queries
 {
     public class GetAllVacanciesQuery : PagedQuery, IRequest<PagedResult<VacancyDto>>
     {
-        public Guid Id { get; set; }
     }
 }
