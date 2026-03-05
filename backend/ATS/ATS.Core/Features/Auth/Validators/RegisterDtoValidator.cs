1e69c0ae7c3c26758e2c2c19bc6b3c773c36a431
echpochmak770 <echpochmak770@gmail.com>
2026-03-05 15:34:04 +0300

    Update validators

--- backend/ATS/ATS.Core/Features/Auth/Validators/RegisterDtoValidator.cs
+++ backend/ATS/ATS.Core/Features/Auth/Validators/RegisterDtoValidator.cs
@@ -12,24 +12,27 @@ namespace ATS.Core.Features.Auth.Validators
         {
             RuleFor(x => x.Email)
                 .NotEmpty().WithMessage("Email is required")
-                .EmailAddress().WithMessage("Invalid email format")
-                .MaximumLength(256);
-
-            RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Password is required")
-                .MinimumLength(8).WithMessage("Password must be at least 8 characters")
-                .MaximumLength(128);
+                .EmailAddress().WithMessage("Invalid email format");
 
             RuleFor(x => x.FirstName)
                 .NotEmpty().WithMessage("First name is required")
-                .MaximumLength(64);
+                .Length(2, 50).WithMessage("First name must be between 2 and 50 characters");
 
             RuleFor(x => x.LastName)
                 .NotEmpty().WithMessage("Last name is required")
-                .MaximumLength(64);
+                .Length(2, 50).WithMessage("Last name must be between 2 and 50 characters");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
+                .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+                .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+                .Matches(@"[0-9]").WithMessage("Password must contain at least one number")
+                .Matches(@"[\!\?\*\.]").WithMessage("Password must contain at least one special character (!?*.)");
 
             RuleFor(x => x.Phone)
-                .MaximumLength(20)
+                .Matches(@"^\+?[1-9]\d{1,14}$")
+                .WithMessage("Invalid phone number format")
                 .When(x => !string.IsNullOrEmpty(x.Phone));
         }
     }
