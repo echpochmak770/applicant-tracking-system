5c593503db18e9e079ba1adbdebac4b159fc76fc
Anastasiya <anastasiya.artishevskaya@slmax.by>
2026-03-08 00:19:05 +0300

    style: header

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -31,27 +31,24 @@ export function Header({ user }: HeaderProps) {
           <div className="flex h-9 w-9 items-center justify-center rounded-lg bg-primary shadow-lg shadow-primary/20">
             <LayoutDashboard className="h-5 w-5 text-primary-foreground" />
           </div>
-          <div className="flex gap-1">
-          <span className="text-xl font-bold tracking-tight">
-            ATS 
+          <span className="text-lg font-bold tracking-tight">
+            ATS System
           </span>
-          <span className="text-lg text- font-medium text-primary">System</span>
-          </div>
 
         </div>
 
         <div className="flex items-center gap-5">
           <div className="flex items-center gap-4 group cursor-default">
             <div className="hidden flex-col items-end md:flex">
-              <span className="text-2xl font-semibold leading-none transition-colors">
+              <span className="text-lg font-semibold leading-none transition-colors">
                 {user?.fullName || "Гость системы"}
               </span>
-              <span className="text-[12px] text-accent font-medium mt-1 uppercase tracking-widest">
+              <span className="text-sm text-accent font-medium mt-1 uppercase tracking-widest">
                 {user?.email || "Awaiting Auth"}
               </span>
             </div>
             
-            <div className="flex h-15 w-15 items-center justify-center rounded-full bg-gradient-to-br from-chart-1 to-chart-2 text-lg font-bold text-white border border-white/20 shadow-inner">
+            <div className="flex h-13 w-13 items-center justify-center rounded-full bg-gradient-to-br from-chart-1 to-chart-2 text-md font-bold text-white border border-white/20 shadow-inner">
               {initials}
             </div>
           </div>
